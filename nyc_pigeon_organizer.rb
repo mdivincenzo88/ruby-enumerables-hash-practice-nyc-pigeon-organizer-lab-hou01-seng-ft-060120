ead7ec5c8de64077a3a41d30457557d6107fd9b9
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-23 02:08:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-data.each_with_object({}) do [{key, val}]
+data.each_with_object({}) do |{key, value}, final_array|
+  value.each do |inner_key, names|
+    names.each do |name|
+      if !final_array[name]
+        final_array[name] = {}
+      end
+      if !final_array[name][key]
+        final_array[name][key] = []
+      end
+      
 end
